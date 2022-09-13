255b017fbec7adf3798d9bd9daae33c2b4b2b01f
sgupta70 <sgupta70@charlottesvilleschools.org>
2022-09-13 15:34:31 -0400

    code for servo

--- myservo.py
+++ myservo.py
@@ -1,1 +1,22 @@
-#++++ test again
+#Sahana Gupta
+#CircuitPython Servo 
+#This code will turn on a motor and make it rotate 180 degrees back and forth
+
+"""CircuitPython Essentials Servo standard servo example"""
+import time 
+import board 
+import pwmio
+from adafruit_motor import servo
+
+
+pwm = pwmio.PWMOut(board.D3, duty_cycle=2 ** 15, frequency=50) #using the PWMOut function within the pwmio library 
+
+
+my_servo = servo.Servo(pwm)
+
+while True:
+    for angle in range(0, 180, 5): #the servo rotates 180 degrees 
+        my_servo.angle = angle
+        time.sleep(0.05) #pauses for 0.05 seconds
+    for angle in range(180, 0, -5): #the servo rotates 180 degrees ther other way 
+        time.sleep(0.05) #pauses for 0.05 seconds
