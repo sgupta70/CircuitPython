c71ac9988c27d5d4b6d2d65e00da37b271e7640c
sgupta70 <sgupta70@charlottesvilleschools.org>
2022-09-19 15:53:03 -0400

    working code

--- distancesensor.py
+++ distancesensor.py
@@ -6,6 +6,7 @@ import board
 import time 
 import neopixel
 import adafruit_hcsr04
+import simpleio
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
@@ -14,24 +15,45 @@ dot.brightness = 0.1
 
 
 while True:
-   distance = sonar.distance
-   (print (distance))
-     if distance < 5:
-    r = 255
-    g = 0 
-    b = 0 
-    elif distance > 5:
 
+    try:
+        distance = sonar.distance
+        (print (distance))
+    except RuntimeError:
+        print("Retrying!")
+        r = 0 
+        b = 0 
+        g = 255
+        time.sleep(0.1)
 
-
+    if distance < 5:
+        r = 255
+        g = 0 
+        b = 0 
+        r = int(r) 
+        g = int(g)
+        b = int(b)
+        print("<5")
+    elif distance > 5 and distance < 20:
+        r = 0
+        b = 0
+        g = 255
+        r = int(r) 
+        g = int(g)
+        b = int(b)
+        print(">5<20")
     elif distance > 20: 
         r = 0 
         g = 0 
         b = 255 
+        r = int(r) 
+        g = int(g)
+        b = int(b)
+        print(">20")
 
-    dot.fill((255, 51, 153))
-    try:
-        print((sonar.distance,))
-    except RuntimeError:
-        print("Retrying!")
-    time.sleep(0.1)
+    
+    
+
+    print (r,g,b)
+    dot.fill((r,g,b))
+    time.sleep (0.05)
